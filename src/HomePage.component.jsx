80a479f88d0dab6682c614b0d9fd4fb881b50f9d
khushi1706 <git-commit-address>
2020-03-15 22:34:40 +0530

    Initial commit with homepage component and sass setup

---
+++ src/HomePage.component.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./HomePage.styles.scss";
+export const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="sub-title">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">JACKETS</h1>
+          <span className="sub-title">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">SNEAKERS</h1>
+          <span className="sub-title">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">WOMEN CORNER</h1>
+          <span className="sub-title">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">MEN CORNER</h1>
+          <span className="sub-title"> SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
