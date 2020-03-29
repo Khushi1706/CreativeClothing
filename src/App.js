0e4cd0d51cbd0cbc9314733dd9e58f25d03a7df0
khushi1706 <git-commit-address>
2020-03-29 15:56:02 +0530

    Created seperate components for directory and menu items & styled the menu-items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { HomePage } from './HomePage.component.jsx'
+import { HomePage } from './pages/HomePage.component'
 
 function App() {
   return (
---
+++ src/pages/HomePage.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./HomePage.styles.scss";
+import Directory from "../components/directory/directory.component";
+export const HomePage = () => (
+  <div className="homepage">
+    <Directory></Directory>
+  </div>
+);
