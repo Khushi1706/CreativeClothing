5eed58ee2f906f38598d2e39e098f0b8bdd7bbe3
khushi1706 <git-commit-address>
2020-04-05 19:26:26 +0530

    Added basic routing functionality using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage.component'
+
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
       <div>
-        <HomePage></HomePage>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/hats' component={HatsPage} />
+        </Switch>
       </div>
     </div>
   );
